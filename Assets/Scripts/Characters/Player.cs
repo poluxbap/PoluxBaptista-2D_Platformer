b00cc9076331447df3c7aa02b5def82334f65a46
poluxbap <polux.codes@gmail.com>
2021-08-21 01:58:22 -0300

    New improved movement && animation on prototype

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -6,71 +6,33 @@ using DG.Tweening;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
+    public LayerMask groundLayer;
+    public Animator anim;
 
     [Header("Physics")]
     public float speed;
     public float runSpeed;
-    public Vector2 friction = new Vector2(.1f, 0);
     public float jumpForce;
-
-    [Header("Animation jump")]
-    public float jumpScaleX = .7f;
-    public float jumpScaleY = 1.5f;
-    public float jumpAnimationDuration = .3f;
-    public Ease jumpEase = Ease.OutBack;
-
-    [Header("Animation land")]
-    public float landScaleX = 1.5f;
-    public float landScaleY = .7f;
-    public float landAnimationDuration = .3f;
-    public Ease landEase = Ease.OutBack;
 
     private bool _isRunning = false;
 
     void Update()
     {
-        MovemenntX();
-        MovementY();
-    }
-
-    public void MovemenntX()
-    {
         _isRunning = Input.GetKey(KeyCode.LeftControl);
 
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            myRigidbody2D.velocity = new Vector2(_isRunning ? runSpeed : speed, myRigidbody2D.velocity.y);
-        }
-        else if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            myRigidbody2D.velocity = new Vector2(_isRunning ? -runSpeed : -speed, myRigidbody2D.velocity.y);
-        }
+        myRigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * (_isRunning ? runSpeed : speed), myRigidbody2D.velocity.y);
 
-        if(myRigidbody2D.velocity.x > 0)
-        {
-            myRigidbody2D.velocity -= friction;
-        }
-        else if(myRigidbody2D.velocity.x < 0)
-        {
-            myRigidbody2D.velocity += friction;
-        }
-    }
-
-    public void MovementY()
-    {
-        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && onGround())
         {
             myRigidbody2D.velocity = Vector2.up * jumpForce;
-            myRigidbody2D.transform.localScale = Vector2.one;
-            DOTween.Kill(myRigidbody2D.transform);
-            HadleJumpAnimation();
         }
 
+        anim.SetBool("grounded", onGround());
+        anim.SetFloat("vel", myRigidbody2D.velocity.y);
     }
 
-    public void HadleJumpAnimation()
+    private bool onGround()
     {
-        myRigidbody2D.transform.DOScaleX(jumpScaleX, jumpAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
-        myRigidbody2D.transform.DOScaleY(jumpScaleY, jumpAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
+        return Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - (transform.localScale.y / 2f)), transform.localScale.x / 2f, groundLayer);
     }
 }
