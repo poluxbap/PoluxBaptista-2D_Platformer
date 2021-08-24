0c58fbd02eedf0cb106af69c643e262347d09b92
poluxbap <polux.codes@gmail.com>
2021-08-24 00:50:08 -0300

    Camera Easing fixed and animation completed

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform playerTransform;
+    [Header("Cam Params")]
+    public float dampTime = .15f;
+    public float offsetY = 3f;
+    public float offsetX = 0;
 
-    void Start()
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+    public Camera camera;
+
+    void Awake()
     {
-
+        camera = GetComponent<Camera>();
     }
 
     void Update()
     {
-        if(playerTransform != null)
+        if (target)
         {
-            transform.position = playerTransform.position - (Vector3.forward * 10);
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta + new Vector3(offsetX, offsetY, 0);
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
     }
 }
